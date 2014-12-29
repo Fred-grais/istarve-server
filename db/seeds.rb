1c3cba18030665d25bc8977835542eb4e682433e
Fred-grais <frederic.grais@gmail.com>
2014-12-29 22:39:46 +0100

    Seed Restaurantcomments

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,7 +7,8 @@
 #   Mayor.create(name: 'Emanuel', city: cities.first)
 logger = Logger.new(STDOUT)
 users = ['jean.pierre@gmail.com', 'delvian@hotmail.fr', 'android@android.fr', 'christian.loble@pmail.gr', 'claire.montfre@utt.fr']
-
+comments = ['Splendid work you have here.', 'Nice Restaurant!', 'Cool, cool, cool', 'Awesome', 'Ate there, delicious', 'Love this place!', 'Best dishes in town!']
+titles = ['My thoughts', 'Loved it', 'Best restaurant', 'Unbelievable', 'A must see', 'Miam', 'Good']
 created_elems = 0
 users.each do |email|
   unless User.find_by(email: email)
@@ -17,7 +18,8 @@ end
 logger.debug(created_elems.to_s + ' Users seeded')
 
 
-created_elems = 0
+created_ratings = 0
+created_comments = 0
 Restaurant.all.each do |restaurant|
   5.times do
     u = User.find_by(email: users.sample)
@@ -25,8 +27,13 @@ Restaurant.all.each do |restaurant|
     puts "user:" + u.id.to_s
     unless RestaurantRating.find_by(restaurant_id: restaurant.id, user_id: u.id)
       restaurant.ratings.create(rating: rand(1..5), user_id: u.id)
-      created_elems += 1
+      created_ratings += 1
+    end
+    unless RestaurantComment.find_by(restaurant_id: restaurant.id, user_id: u.id)
+      restaurant.comments.create(title: titles.sample, body: comments.sample, user_id: u.id)
+      created_comments += 1
     end
   end
 end
-logger.debug(created_elems.to_s + ' Restaurant Ratings seeded')
+logger.debug(created_ratings.to_s + ' Restaurant Ratings seeded')
+logger.debug(created_comments.to_s + ' Restaurant Comments seeded')
