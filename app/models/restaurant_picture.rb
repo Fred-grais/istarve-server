837cee01952b4cb683498bc55555c366b95d8c7b
Fred-grais <frederic.grais@gmail.com>
2015-01-02 17:44:54 +0100

    Fix try

--- app/models/restaurant_picture.rb
+++ app/models/restaurant_picture.rb
@@ -3,7 +3,7 @@ class RestaurantPicture < ActiveRecord::Base
   has_attached_file :image, :styles => { :medium => "300x300>", :small => "100x100>" }, :default_url => "missing_picture.png"
   validates_attachment_content_type :image, :content_type => /\Aimage\/.*\Z/
 
-  validates :restaurant_id
+  validates :restaurant_id, presence: true
 
   belongs_to :restaurant
 end
